82bc635a10632200b10ae081ac777992b95ccfa6
linetlucy-genchabe <linetlucy21@gmail.com>
2022-06-01 21:01:12 +0300

    add movie function

--- netflixclone/views.py
+++ netflixclone/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+def index(request):
+    # movies=Movie.objects.all()
+    poster_url= "https://image.tmdb.org/t/p/w500/"
+    response = requests.get('https://api.themoviedb.org/3/movie/popular?api_key=969509fb19ee305da27532300d83a5ae')
+    movies_rosponse = response.json()
+    # print(movies_rosponse)
+    movies=movies_rosponse['results']
+    # posters=movies.poster_path
+    # posters=movies['poster_path']
+    # print(movies)
+    # poster=poster_url + posters
+    return render(request, 'index.html',{'movies':movies,})
+    # 'https://api.themoviedb.org/3/movie/popular?api_key=969509fb19ee305da27532300d83a5ae'
